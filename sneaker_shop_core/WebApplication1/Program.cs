725452304dff241158ddb36f312437d0fbfa0e0a
anhquan99 <anhquanala@gmail.com>
2022-01-19 06:34:30 +0700

    update UI
    Implement more function to UI

--- sneaker_shop_core/WebApplication1/Program.cs
+++ sneaker_shop_core/WebApplication1/Program.cs
@@ -27,7 +27,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
