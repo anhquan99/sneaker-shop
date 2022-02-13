90438b84ead6cd38814461902a23718ee6f2b4ee
anhquan99 <anhquanala@gmail.com>
2022-02-13 23:47:49 +0700

    update

--- sneaker_shop_core/WebApplication1/Entities/BrandSilhouette.cs
+++ sneaker_shop_core/WebApplication1/Entities/BrandSilhouette.cs
@@ -3,7 +3,7 @@
     public class BrandSilhouette
     {
         public string NameId { get; set; }
-        public string ? Story { get; set; }
+        public string Story { get; set; }
         public string Icon { get; set; }
         public virtual List<Product> Products { get; set; }
         public DateTime CreatedAt { get; set; }
