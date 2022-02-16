6d5cbc900e1ee6e52315b005471c4297b5044048
anhquan99 <anhquanala@gmail.com>
2022-02-16 15:13:08 +0700

    update UI

---
+++ sneaker_shop_core/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("/Product")]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;    
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;   
+        }
+        [Route("Detail/{ProductName}")]
+        public IActionResult ProductDetail(string ProductName)
+        {
+            _logger.LogWarning(0, "ProductName: " + ProductName);
+            return View();
+        }
+
+    }
+}
---
+++ sneaker_shop_core/WebApplication1/ViewComponents/ProductListViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.ViewComponents
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
